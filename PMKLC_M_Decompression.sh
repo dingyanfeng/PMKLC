b9cc158d9c08bafc483415eb49f469af8fc596c1
dingyanfeng <104774455+dingyanfeng@users.noreply.github.com>
2025-01-06 15:53:21 +0800

    Update PMKLC_M_Decompression.sh

--- PMKLC_M_Decompression.sh
+++ PMKLC_M_Decompression.sh
@@ -7,12 +7,12 @@
 FILE=$1       # 文件名
 GPU=$2        # 算法运行在哪一块GPU
 K=$3          # 窗口大小，对应文中k
-W=$4          # 步长，对应文中w
+W=$4          # 步长，对应文中s
 MODEL_PATH=$5 # SPuM
 BASE=${FILE##*/}
 BASE=${BASE%.*}
 MODEL_LIST_NUM=$(grep -oP '"model_mode_num":\s*\K\d+' "${BASE}.params")
-OUTPUT=${BASE}_recover                        # 根据编码方案计算的输出的压缩文件名
+OUTPUT=${BASE}_recover
 static_public_model_path=${MODEL_PATH}"/all_model_${K}_${W}" # Static Public Model所在的文件路径
 
 echo "-------Decompressing ${BASE} with model-list-number:${MODEL_LIST_NUM}-------"
